63819cad2b160ff51e6dcec1efd8316ea8b05152
Sammy Samkough <sammysamkough@gmail.com>
2020-05-06 04:52:05 -0400

    Updated homepage to show navbar and chart items

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Row, Col, Typography } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+
+const App = () => {
+  const { Title } = Typography;
   return (
-    <div className="app">
-      <p>njcoronavirus admin</p>
-    </div>
+    <Router>
+      <Row className="admin-row">
+        <Col span={2} className="navbar-col">
+          <NavBar />
+        </Col>
+        <Col span={22} className="content-col">
+          <Title level={2}>
+            New Jersey Coronavirus Map Admin{" "}
+            <FontAwesomeIcon icon={faGlobeAmericas} size="lg" />
+          </Title>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Col>
+      </Row>
+    </Router>
   );
-}
+};
 
 export default App;
